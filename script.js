52fb97d04f9845eab20787da83d58b492baa8020
OOnsOng <162480350+OOnsOng@users.noreply.github.com>
2024-06-19 12:47:56 +0900

    사진에 링크연결 문제 해결

---
+++ script.js
@@ -0,0 +1,5 @@
+document
+  .getElementById('clickableImage')
+  .addEventListener('click', function () {
+    window.location.href = 'http://127.0.0.1:5500/src/project.html'; // 이동할 URL을 여기에 입력하세요.
+  });
